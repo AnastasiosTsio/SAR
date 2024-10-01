1a7a4f18acbf89913486d9fe502dfab2c9890e7f
Anastasios TSIOMPANIDIS <anastasios.tsiompanidis@etu.univ-grenoble-alpes.fr>
2024-10-01 20:38:22 +0200

    Added abstract classes and tests

---
+++ task2/src/abstracts/QueueBroker.java
@@ -0,0 +1,13 @@
+package abstracts;
+
+import task1.Broker;
+
+public abstract class QueueBroker {
+	public Broker broker;
+	public QueueBroker(Broker broker){
+		this.broker = broker;
+	}
+	abstract String name();
+	abstract MessageQueue accept(int port);
+	abstract MessageQueue connect(String name, int port);
+}
